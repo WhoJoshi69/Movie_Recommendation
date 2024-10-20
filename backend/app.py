fcbdd9f2887c2ff642dfd6b09515d623ed8b6c94
Darshit <joshidarshit69@gmail.com>
2024-10-20 14:43:44 +0530

    added animations in poster on hover

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,22 @@
+import asyncio
+import time
+import logging
+
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 import httpx
 import os
 from dotenv import load_dotenv
+import requests
+
+from backend.constants import GENRES
+from backend.movie_gen import createPrompts
 
 load_dotenv()
+
+# Configure logging
+logging.basicConfig(level=logging.ERROR)
+logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
@@ -23,45 +35,65 @@ TMDB_BASE_URL = "https://api.themoviedb.org/3"
 
 async def fetch_movies(url):
     async with httpx.AsyncClient() as client:
-        response = await client.get(url)
-        response.raise_for_status()
-        return response.json()
+        try:
+            response = await client.get(url)
+            response.raise_for_status()
+            return response.json()
+        except httpx.HTTPStatusError as e:
+            logger.error(f"HTTP error occurred while fetching movies: {e}")
+            raise
+        except Exception as e:
+            logger.error(f"An error occurred while fetching movies: {e}")
+            raise
+
+
+async def fetch_all_movies(urls):
+    try:
+        tasks = [fetch_movies(url) for url in urls]
+        responses = await asyncio.gather(*tasks)
+        return [movie["results"][0] for movie in responses if movie["results"]]
+    except Exception as e:
+        logger.error(f"An error occurred while fetching all movies: {e}")
+        raise
 
 
 @app.get("/recommend")
 async def get_movies(query: str):
     try:
-        search_url = f"{TMDB_BASE_URL}/search/movie?api_key={TMDB_API_KEY}&query={query}&language=en-US&page=1&include_adult=false"
-        data = await fetch_movies(search_url)
+        start_time = time.time()
+        content = createPrompts(query)
+        content = query + "," + content
+
+        search_urls = [f"{TMDB_BASE_URL}/search/movie?api_key={TMDB_API_KEY}&query={record}&language=en-US&page=1&include_adult=true"
+                       for record in content.split(",")]
+        movies_data = await fetch_all_movies(search_urls)
 
         movies = []
-        for movie in data['results'][:15]:  # Limit to 15 movies
-            poster_path = movie['poster_path']
-            poster_url = f"https://image.tmdb.org/t/p/w200{poster_path}" if poster_path else None
-
-            movies.append({
-                "id": movie['id'],
-                "title": movie['title'],
-                "year": movie['release_date'][:4] if movie['release_date'] else None,
-                "genre_ids": movie['genre_ids'],
-                "posterUrl": poster_url,
-                "overview": movie['overview']
-            })
-
-        # Fetch genres
-        genres_url = f"{TMDB_BASE_URL}/genre/movie/list?api_key={TMDB_API_KEY}&language=en-US"
-        genres_data = await fetch_movies(genres_url)
-        genres = {genre['id']: genre['name'] for genre in genres_data['genres']}
+        for movie in movies_data:
+            poster_path = movie["poster_path"]
+            poster_url = f"https://image.tmdb.org/t/p/w600_and_h900_bestv2/{poster_path}" if poster_path else None
+            if poster_url:
+                movies.append({
+                    "id": movie['id'],
+                    "title": movie['title'],
+                    "year": movie['release_date'][:4] if movie['release_date'] else None,
+                    "genre_ids": movie['genre_ids'],
+                    "posterUrl": poster_url,
+                    "overview": movie['overview']
+                })
 
         # Add genre names to movies
         for movie in movies:
-            movie['genres'] = [genres[genre_id] for genre_id in movie['genre_ids'] if genre_id in genres]
+            movie['genres'] = [GENRES[genre_id] for genre_id in movie['genre_ids'] if genre_id in GENRES]
             del movie['genre_ids']
-
-        return {"movies": movies}
+        print(f"time taken is {time.time() - start_time}")
+        return {"movies": movies[1:],
+                "given_movie": movies[0]}
     except httpx.HTTPStatusError as e:
+        logger.error(f"HTTP error occurred: {e}")
         raise HTTPException(status_code=e.response.status_code, detail=str(e))
     except Exception as e:
+        logger.error(f"An unexpected error occurred: {e}")
         raise HTTPException(status_code=500, detail=str(e))
 
 
--- backend/movie_gen.py
+++ backend/movie_gen.py
@@ -1,8 +1,10 @@
+import os
+
 from groq import Groq
 from pip._internal.utils.misc import enum
 
 client = Groq(
-    api_key="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",
+    api_key=os.getenv("GROQ_API_KEY"),
 )
 
 
@@ -26,26 +28,13 @@ def createPrompts(prompt):
                 "content": """i want you to be my movie recommendation engineer, who have knowledge of all the explicit 
                 - non explicit. rated - non rated - r rated movies in the world. when i give you a name of a movie, 
                 you will in respond give me 15 movies and series combined or non combined with having really really
-                 similar plot or environment or story. you will represent the response in python list format nothing
-                  extra information just the movie name, release year in bracket nothing extra at all
-                  - reminding you once again i need response in python list format nothing extra.
-                  - example response: [
-    "Emmanuelle (1974)",
-    "The Story of O (1975)",
-    "The Night Porter (1974)",
-    "Salon Kitty (1976)",
-    "Caligula (1979)",
-    "Fellini Satyricon (1969)",
-    "The Libertine (1968)",
-    "The Damned (1962)",
-    "Theorem (1968)",
-    "Belle de Jour (1967)",
-    "Juliet of the Spirits (1965)",
-    "The Servant (1963)",
-    "Secrets of a Chambermaid (1998)",
-    "Eyes Wide Shut (1999)",
-    "Mulholland Drive (2001)"
-]
+                 similar plot or environment or story. you will represent the response in comma seperated strings format nothing
+                  extra information just the movie name nothing extra at all
+                  - reminding you once again i need response in comma seperated strings format nothing extra.
+                  - nothing extra not at all. response format should exactly look like example. 
+                  - don't even say here are your required data. just give me those names only and only
+                  - example response: 
+    response-start: "Emmanuelle,The Story of O,The Night Porter,Salon Kitty,Caligula,Fellini Satyricon,The Libertine,The Damned,Theorem,Belle de Jour,Juliet of the Spirits,The Servant,Secrets of a Chambermaid,Eyes Wide Shut,Mulholland Drive" - response-end
                   """
 
             },
@@ -63,25 +52,6 @@ def createPrompts(prompt):
 
     # Return the generated prompt
     return completion.choices[0].message.content
-
-
-response = [
-    "Emmanuelle (1974)",
-    "The Story of O (1975)",
-    "The Night Porter (1974)",
-    "Salon Kitty (1976)",
-    "Caligula (1979)",
-    "Fellini Satyricon (1969)",
-    "The Libertine (1968)",
-    "The Damned (1962)",
-    "Theorem (1968)",
-    "Belle de Jour (1967)",
-    "Juliet of the Spirits (1965)",
-    "The Servant (1963)",
-    "Secrets of a Chambermaid (1998)",
-    "Eyes Wide Shut (1999)",
-    "Mulholland Drive (2001)"
-]
-response = createPrompts("celestine 1974")
-print(response)
-print(type(enum(response)))
+# response = createPrompts("celestine 1974")
+# print(response)
+# print(type(enum(response)))
